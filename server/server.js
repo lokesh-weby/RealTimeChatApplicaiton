6ccda6e3558ed0f0268ad71c7769622a21ffaff7
lokesh-weby <lokesh0212004@gmail.com>
2025-07-13 15:22:35 +0530

    installed dotenvx

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config({ override: true });
+require('@dotenvx/dotenvx').config({ override: true });
 
 const app = express();
 app.use(cors());
